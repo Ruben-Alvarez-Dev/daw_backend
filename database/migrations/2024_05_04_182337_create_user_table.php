9e4ec3e9b8b3c977a18e94f28a6f29de5f76f700
Rubén Álvarez <info@rubenalvarez.dev>
2024-05-04 20:32:29 +0200

    schemes modified

--- database/migrations/2024_05_04_182337_create_user_table.php
+++ database/migrations/2024_05_04_182337_create_user_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
+            $table->string('surname');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
--- database/migrations/2024_05_04_182343_create_reservation_table.php
+++ database/migrations/2024_05_04_182343_create_reservation_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('reservation', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('table_id');
+            $table->date('reservation_date');
+            $table->time('start_time');
+            $table->integer('num_guests');
+            $table->string('status');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('table_id')->references('id')->on('table');
         });
     }
 
