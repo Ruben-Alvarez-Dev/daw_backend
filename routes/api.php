077ba97d3b339630f75b391a91df280fb3eb6c79
Rubén Álvarez <info@rubenalvarez.dev>
2024-05-04 19:34:03 +0200

    routing

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/tables", function () {
-    return 'Table list'
-});
+
+Route::get('/tables', function () {return 'Tables';});
+Route::get('/tables/{id}', function () {return 'Tables';});
+Route::post('/tables', function () {return 'Tables';});
+Route::put('/tables/{id}', function () {return 'Tables';});
+Route::delete('/tables/{id}', function () {return 'Tables';}); 
+
+Route::get('/users', function () {return 'Users';});
+Route::get('/users/{id}', function () {return 'Users';});
+Route::post('/users', function () {return 'Users';});
+Route::put('/users/{id}', function () {return 'Users';});
+Route::delete('/users/{id}', function () {return 'Users';});
+
+Route::get('/reservations', function () {return 'Orders';});
+Route::get('/reservations/{id}', function () {return 'Orders';});
+Route::post('/reservations', function () {return 'Orders';});
+Route::put('/reservations/{id}', function () {return 'Orders';});
+Route::delete('/reservations/{id}', function () {return 'Orders';});
