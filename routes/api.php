f059c796388e60055afb83927dd0ba4e0013be1e
Ruben Alvarez <ruben.alvaresz.dev@gmail.com>
2024-12-31 09:08:35 +0100

    new blank project

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,19 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TableController;
-use App\Http\Controllers\ReservationController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// User routes
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-// Table routes
-Route::get('/tables', [TableController::class, 'index']);
-Route::post('/tables', [TableController::class, 'store']);
-Route::get('/tables/{table}', [TableController::class, 'show']);
-Route::put('/tables/{table}', [TableController::class, 'update']);
-Route::delete('/tables/{table}', [TableController::class, 'destroy']);
-
-// Reservation routes
-Route::get('/reservations', [ReservationController::class, 'index']);
-Route::post('/reservations', [ReservationController::class, 'store']);
-Route::get('/reservations/{reservation}', [ReservationController::class, 'show']);
-Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
-Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
